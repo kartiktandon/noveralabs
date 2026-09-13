f6b34a6e04feb92de47e49656dd893d7ddb21dae
tandonk <kartik.tandon@tabcorp.com.au>
2026-09-13 21:19:42 +0530

    vercel. issue

--- vite.vercel.config.ts
+++ vite.vercel.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from '@tailwindcss/postcss';
 import { nitro } from 'nitro/vite';
+import path from 'node:path';
 import vinext from 'vinext';
 import { defineConfig } from 'vite';
 
@@ -7,6 +8,17 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss()],
+    },
+  },
+  resolve: {
+    alias: {
+      tailwindcss: path.resolve('node_modules/tailwindcss/index.css'),
+      'tw-animate-css': path.resolve(
+        'node_modules/tw-animate-css/dist/tw-animate.css',
+      ),
+      'shadcn/tailwind.css': path.resolve(
+        'node_modules/shadcn/dist/tailwind.css',
+      ),
     },
   },
   plugins: [vinext(), nitro()],
